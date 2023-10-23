7e747c5c6cc36713ae1a1913cfede60cc820c0ac
Ky-Anh Nguyen <140636813+Kyros0718@users.noreply.github.com>
2023-10-23 18:05:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-render_template("index.html")
-enable_script = False
-if enable_script:
-    @app.route("/")
-    def index():
-        return render_template("index.html")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
