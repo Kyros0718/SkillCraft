88391839ec978d7b7c3418f75364ced740bbdda3
Ky-Anh Nguyen <140636813+Kyros0718@users.noreply.github.com>
2023-10-23 23:10:49 -0400

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,32 @@
+const form = document.getElementById("myForm");
+const skilldropdown = document.getElementById("skill-dropdown");
+var inputElement = document.getElementById("InputField");
+var inputValue = inputElement.value;
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  if (inputElement.value.trim() !== '') {
+    showDropdown(["Beginner", "Intermediate", "Expert"]);
+  }
+  else{
+    skilldropdown.style.display = "none"
+  }
+});
 
+function showDropdown(items) {
+    skilldropdown.style.display = "block";
+    skilldropdown.innerHTML = ""; // Clear previous options
+
+    items.forEach(itemText => {
+        const resultItem = document.createElement("div");
+        resultItem.classList.add("result-item");
+        resultItem.textContent = itemText;
+        resultItem.addEventListener("click", () => onItemClicked(itemText));
+        skilldropdown.appendChild(resultItem); // Corrected the variable name here
+    });
+}
+
+function onItemClicked(selectedItem) {
+  if (selectedItem !== "Beginner" && selectedItem !== "Intermediate" && selectedItem !== "Expert") {
+    alert("loading...");
+  }
+}
