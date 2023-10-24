faae15b667109a16af5549c4b4d5af650d2c23ba
KyAnh Nguyen <kyanhnguyen513@gmail.com>
2023-10-24 01:03:04 -0400

    modify files

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+import openai
+import json
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+@app.route("/instruction.html")
+def instruction():
+    return render_template("instruction.html")
