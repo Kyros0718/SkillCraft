d0076ef77577b7a54a22b642e2f2f7fca6c20e7e
KyAnh Nguyen <kyanhnguyen513@gmail.com>
2023-10-24 01:50:19 -0400

    Updated Search Bar to interact with subject

--- static/script.js
+++ static/script.js
@@ -1,22 +1,29 @@
 const form = document.getElementById("myForm");
 const dropdown = document.getElementById("dropdown");
-const ideadropdown = document.getElementById("idea-dropdown");
+const questionsubject = document.getElementById("subject1");
+const skillsubject = document.getElementById("subject2");
+const projectsubject = document.getElementById("subject3");
 var inputElement = document.getElementById("InputField");
 var inputValue = inputElement.value;
 form.addEventListener("submit", function(event) {
   event.preventDefault();
   if (inputElement.value.trim() !== '') {
     showDropdown(["Beginner", "Intermediate", "Expert"]);
+    questionsubject.style.display = "none";
+    skillsubject.style.display = "block";
   }
-  else{
-    dropdown.style.display = "none"
+  else {
+    dropdown.style.display = "none";
+    questionsubject.style.display = "block";
+    skillsubject.style.display = "none";
+    projectsubject.style.display = "none";
+    
   }
 });
 
 function showDropdown(items) {
     dropdown.style.display = "block";
     dropdown.innerHTML = ""; // Clear previous options
-    ideadropdown.innerHTML = ""; // Clear previous options
     
 
     items.forEach(itemText => {
@@ -31,9 +38,13 @@ function showDropdown(items) {
 function onItemClicked(selectedItem) {
   if (selectedItem === "Beginner" || selectedItem === "Intermediate" || selectedItem === "Expert") {
     dropdown.style.display = "none";
+    skillsubject.style.display = "none";
+    projectsubject.style.display = "block";
     showDropdown(["item1","item2","item3","item4","item5","item6","item7","item8","item9","item10",])
   }
   else {
     dropdown.style.display = "none";
+    projectsubject.style.display = "none";
+    questionsubject.style.display = "block";
   }
 }
