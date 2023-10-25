a6dadb800385a40c7c67102168a5211a752ee91c
KyAnh Nguyen <kyanhnguyen513@gmail.com>
2023-10-25 19:52:40 -0400

    fixed backend files

--- back_end/routes.py
+++ back_end/routes.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # BackEnd/routes.py
 
 from flask import Blueprint, render_template
@@ -12,19 +11,3 @@ def index():
 @bp.route('/instruction.html')
 def instruction():
     return render_template('instruction.html')
-||||||| 4839ac8
-=======
-# BackEnd/routes.py
-
-from flask import Blueprint, render_template
-
-bp = Blueprint('main', __name__)
-
-@bp.route('/')
-def index():
-    return render_template('index.html')
-
-@bp.route('/instruction.html')
-def instruction():
-    return render_template('instruction.html')
->>>>>>> 36e7eb6f155ed7b210ba9294c6ac761e4efe5f6a
