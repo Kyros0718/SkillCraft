2654ef4057d30d07268bf2a2525e05f09a1901c7
KyAnh Nguyen <kyanhnguyen513@gmail.com>
2023-10-28 02:37:32 -0400

    Create Class For Skill Levels

--- front_end/static/js/script.js
+++ front_end/static/js/script.js
@@ -1,60 +1,64 @@
 const form = document.getElementById("myForm");
 const dropdown = document.getElementById("dropdown");
-const questionsubject = document.getElementById("subject1");
-const skillsubject = document.getElementById("subject2");
-const projectsubject = document.getElementById("subject3");
+const [questionsubject, skillsubject, projectsubject] = [
+  document.getElementById("subject1"),
+  document.getElementById("subject2"),
+  document.getElementById("subject3")]
 const searchbar = document.getElementsByClassName("input-container")
 
 var inputElement = document.getElementById("InputField");
 var inputValue = inputElement.value;
 
-class SkillLevel {
-  /**
-   * This is for differentiating skill levels from text strings
-   * @param {String} Level 
-   */
-  constructor(Level) {
-    this._skillLevel = Level;
-  }
 
-  get skillLevel() {
-    return this._skillLevel;
-  }
-}
-
-const beginner = new SkillLevel("Beginner");
-const intermediate = new SkillLevel("Intermediate");
-const expert = new SkillLevel("Expert");
-
-const skillLevels = [beginner, intermediate, expert];
-
+//RENDER WEB-PAGE AFTER LOADING-PAGE 
 window.addEventListener('load', function () {
-  // Simulate a delay to keep the loading screen visible for a longer period
+
   setTimeout(function () {
       const loadingContainer = document.querySelector('.loading-container');
       const content = document.querySelector('.website-container');
 
       loadingContainer.style.display = 'none';
       content.style.display = 'block';
-  }, 2500); // 1000 milliseconds = 1.0 seconds
+  }, 0); // 1000 milliseconds = 1.0 seconds
 });
 
 
+class SkillLevel {
+  constructor(Level) {
+    this._skillLevel = Level;
+  }
+  get skillLevel() {
+    return this._skillLevel;
+  }
+}
+
+const skillLevels = [
+  new SkillLevel("Beginner"),
+  new SkillLevel("Intermediate"),
+  new SkillLevel("Expert")];
+
+const skillLevelNames = skillLevels.map(level => level.skillLevel);
+
 form.addEventListener("submit", function(event) {
   event.preventDefault();
-  if (inputElement.value.trim() !== '') {
-    showDropdown(["Beginner", "Intermediate", "Expert"]);
+  if (inputElement.value.trim() !== '') { //If Input is not Empty, Render Skill Levels
+    
+    showDropdown(skillLevelNames);
+
     questionsubject.style.display = "none";
-    projectsubject.style.display = "none";
     skillsubject.style.display = "block";
+    projectsubject.style.display = "none";
   }
-  else {
+  else { //If Empty, dont Render Anything
+
     dropdown.style.display = "none";
+
     questionsubject.style.display = "block";
     skillsubject.style.display = "none";
     projectsubject.style.display = "none";
   }
-  if (dropdown.style.display !== "none") {
+
+  if (dropdown.style.display !== "none") { //If Dropdown, Highlight Border
     for (let i = 0; i < searchbar.length; i++) {
       searchbar[i].style.border = "1px solid white";
     }
@@ -76,8 +80,7 @@ function showDropdown(items) {
 }
 
 function onItemClicked(selectedItem) {
-  if (selectedItem instanceof SkillLevel) {
-    alert("this works");
+  if (skillLevelNames.includes(selectedItem)) {
     dropdown.style.display = "none";
     skillsubject.style.display = "none";
     projectsubject.style.display = "block";
