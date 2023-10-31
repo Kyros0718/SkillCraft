8bbc76b42d0523b200bb87585b11a84765a04624
Ky-Anh Nguyen <140636813+Kyros0718@users.noreply.github.com>
2023-10-31 15:35:42 -0400

    Update script.js

--- front_end/static/js/script.js
+++ front_end/static/js/script.js
@@ -19,7 +19,7 @@ window.addEventListener('load', function () {
 
       loadingContainer.style.display = 'none';
       content.style.display = 'block';
-  }, 0); // 1000 milliseconds = 1.0 seconds
+  }, 2500); // 1000 milliseconds = 1.0 seconds
 });
 
 
