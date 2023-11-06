0df35bd0a97b5a36c7fc6e47c85ef5788cedb359
KyAnh Nguyen <kyanhnguyen513@gmail.com>
2023-11-06 03:27:15 -0500

    Add Walkthrough Feature

--- front_end/static/js/script.js
+++ front_end/static/js/script.js
@@ -1,5 +1,16 @@
+//------------------------------ CONST & VAR ------------------------------
 const rootStyles = getComputedStyle(document.documentElement);
 
+
+//WEBSITE NAV/BODY CONST & VAR_____
+const leftNav = document.getElementById("left-nav");
+const topNav = document.getElementById("top-nav");
+const mainBody = document.getElementById("main-body");
+const logoTop = document.getElementById('logo-top');
+const logoLeft = document.getElementById('logo-left');
+
+
+//PROJECT-SEARCH CONST & VAR_____
 const [questionsubject, skillsubject, projectsubject] = [
   document.getElementById("subject1"),
   document.getElementById("subject2"),
@@ -12,15 +23,21 @@ const dropdown = document.getElementById("dropdown");
 var inputElement = document.getElementById("InputField");
 var inputText = inputElement.textContent;
 
+
+//WALKTHROUGH CONST & VAR_____
 const walkthrough_title = document.getElementById("project-type");
+const expandingSteps = document.getElementById("expanding-steps");
 
-const leftNav = document.getElementById("left-nav")
-const topNav = document.getElementById("top-nav")
-const mainBody = document.getElementById("main-body")
-const logoTop = document.getElementById('logo-top')
-const logoLeft = document.getElementById('logo-left')
 
-//RENDER WEB-PAGE AFTER LOADING-PAGE 
+
+
+
+
+
+//------------------------------ FUNCTIONS ------------------------------
+
+
+//Render [WEB-PAGE] After [LOADING-PAGE] 
 window.addEventListener('load', function () {
   setTimeout(function () { //SET LOADING-TIMER
       const loadingContainer = document.querySelector('.loading-container');
@@ -28,11 +45,14 @@ window.addEventListener('load', function () {
 
       loadingContainer.style.display = 'none';
       content.style.display = 'block';
-  }, 2500); // 1000 milliseconds = 1.0 seconds
+  }, 0); // 1000 milliseconds = 1.0 seconds
 });
 
 
-if (window.innerWidth <= 1000){ //left-NAV defaul display depends on window width
+
+
+//left-NAV Display Toggle ON/OFF
+if (window.innerWidth <= 1000){ //left-NAV default display depends on window width
   leftNav.style.display = 'none';
   topNav.style.width = '100%';
   topNav.style.left = '0';
@@ -59,6 +79,11 @@ logoLeft.addEventListener("click", function() { //Turn On left-Nav
   logoTop.style.display = 'block';
 });
 
+
+
+
+//----- PROJECT-SEARCH____________________
+
 //CLASS OF SKILL-LEVELS FOR PROJECTS
 class SkillLevel {
   constructor(Level) {
@@ -80,6 +105,7 @@ const skillLevelNames = skillLevels.map(level => level.skillLevel);
 //Interaction with the Generate-Button
 searchbar.addEventListener("submit", function(event) {
   event.preventDefault();
+
   if (inputElement.value.trim() !== '') { //If Input is not Empty, Render Skill Levels
     showDropdown(skillLevelNames); //Display Dropdowns Of Skill Levels
 
@@ -96,11 +122,12 @@ searchbar.addEventListener("submit", function(event) {
   }
 
   if (dropdown.style.display !== "none") { //If Dropdown, Highlight Border
-    console.log("WAHTS")
     searchbar.style.border = "1px solid white";
   }
 });
 
+
+//Dropdown Functionality
 function showDropdown(items) {
     dropdown.style.display = "block";
     dropdown.innerHTML = ""; // Clear previous options
@@ -118,11 +145,15 @@ function showDropdown(items) {
     });
 }
 
+var skill_level_choice = '';
+var project_idea_choice = '';
+var project_idea_instruction = [];
+//Items Clicked From Dropdown [SKILL]/[PROJECT]
 function onItemClicked(selectedItem) {
-  if (skillLevelNames.includes(selectedItem)) {
+  if (skillLevelNames.includes(selectedItem)) { //ITEM CHOSEN IS A [SKILL-LEVEL]
     searchButton.style.background = 'grey';
     searchButton.disabled = true;
-
+    skill_level_choice = selectedItem;
     fetch('/project_searching', { //Send User's Input Text and Skill Level to the Server
       method: 'POST',
       body: JSON.stringify({input_text: inputElement.value , skill_level: selectedItem }),
@@ -148,13 +179,119 @@ function onItemClicked(selectedItem) {
     projectsubject.style.display = "block";
     
   }
-  else {
+  else { //ITEM CHOSEN IS A [PROJECT]
+    project_idea_choice = selectedItem;
+    
     walkthrough_title.textContent = selectedItem; //CHANGE WALKTHROUGH TITLE TO PROJECT SELECTED
 
+
+    //CLOSE DROPDOWN DISPLAY 
     searchbar.style.border = "none";
 
     dropdown.style.display = "none";
     projectsubject.style.display = "none";
     questionsubject.style.display = "block";
+
+    fetch('/walkthrough', { //Send User's Project Idea Choice to Server
+      method: 'POST',
+      body: JSON.stringify({input_text: inputElement.value , skill_level: skill_level_choice, project_idea: selectedItem }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(data => { //BACK_END LIST OF PROJECT DROPDOWN
+      project_idea_instruction = data.project_idea_steps;
+      expandingSteps.showExandableSteps(data.project_idea_steps,'rgb(0, 0, 0)');
+    })
+    .catch(error => { //FRONT_END LIST OF PROJECT DROPDOWN
+      project_idea_instruction = ["TEMP_STEP 1","TEMP_STEP 2","TEMP_STEP 3","TEMP_STEP 4","TEMP_STEP 5"];
+      console.error("YOU ARE IN FRONT_END DISPLAY ONLY");
+      expandingSteps.showExandableSteps(["TEMP_STEP 1","TEMP_STEP 2","TEMP_STEP 3","TEMP_STEP 4","TEMP_STEP 5"],'rgb(0, 0, 0)');
+    });
+  }
+}
+
+
+
+
+//----- WALKTHROUGH____________________
+var project_idea_current = '';
+var project_step_help = '';
+HTMLDivElement.prototype.showExandableSteps = function (items, backgroundColor) {
+  this.style.display = "block";
+  this.style.backgroundColor = backgroundColor;
+
+  const steps = document.createElement('div');
+  steps.classList.add("steps");
+  this.innerHTML = ""; // Clear previous options
+  
+  this.appendChild(steps)
+
+
+  items.forEach(itemText => {
+      const stepContainer = document.createElement("div");
+      const stepItem = document.createElement("div");
+      const stepExpandable = document.createElement("div");
+
+      stepContainer.classList.add("step-container");
+      stepItem.classList.add("step-item");
+      stepExpandable.classList.add("step-expandable");
+
+      stepContainer.appendChild(stepItem);
+      stepContainer.appendChild(stepExpandable);
+      stepItem.textContent = itemText;
+      steps.appendChild(stepContainer);
+
+
+      stepItem.addEventListener("click", function() {
+        this.parentNode.classList.toggle('step-expanded');
+        if (this.parentElement.classList.contains('step-expanded')) {
+          const newBackgroundColor = calcNewBackgroundColor(backgroundColor);
+          if (this.parentElement.parentElement.parentNode.id==="expanding-steps"){
+            project_idea_current = itemText
+          };
+          fetch('/expanding-instruction', { //Send User's choice in the steps to Server
+            method: 'POST',
+            body: JSON.stringify({
+              input_text: inputElement.value,
+              skill_level: skill_level_choice,
+              project_idea: project_idea_choice,
+              project_idea_steps: project_idea_instruction,
+              project_idea_currentstep: project_idea_current,
+              project_step_help: itemText}),
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          })
+          .then(response => response.json())
+          .then(data => { //BACK_END LIST OF PROJECT DROPDOWN
+            stepExpandable.showExandableSteps(data.project_step_expand,newBackgroundColor);
+          })
+          .catch(error => { //FRONT_END LIST OF PROJECT DROPDOWN
+            console.error("YOU ARE IN FRONT_END DISPLAY ONLY");
+            stepExpandable.showExandableSteps(["TEMP_EXPAND 1","TEMP_EXPAND 2","TEMP_EXPAND 3","TEMP_EXPAND 4","TEMP_EXPAND 5"],newBackgroundColor);
+          });
+
+
+        } else {
+          stepExpandable.innerHTML = "";
+        }
+      }); 
+  });
+}
+
+function calcNewBackgroundColor(currentColor) {
+
+  const rgbValues = currentColor.match(/\d+/g);
+  if (rgbValues.length === 3) {
+      // Increase the red, green, and blue values by 
+      const change = 20;
+      const newRed = parseInt(rgbValues[0]) + change;
+      const newGreen = parseInt(rgbValues[1]) + change;
+      const newBlue = parseInt(rgbValues[2]) + change;
+      return `rgb(${newRed}, ${newGreen}, ${newBlue})`;
+  } else {
+      return currentColor; 
   }
 }
